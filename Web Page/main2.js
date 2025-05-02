aca8d98e371d054bb4aeb85e9ffb3671c06be942
tka1 <tomi.j.kallio@iki.fi>
2025-05-02 17:49:16 +0300

    Deleted unused function.

--- Web Page/main2.js
+++ Web Page/main2.js
@@ -317,18 +317,7 @@ function setCookie(cname,cvalue,exdays) {
     
  }
 
- function getRandomColor() { //generates random colours and puts them in string
-  var colors = [];
-  for (var i = 0; i < 6; i++) {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var x = 0; x < 6; x++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    colors.push(color);
-  }
-  return colors;
-}
+
 
 
 
